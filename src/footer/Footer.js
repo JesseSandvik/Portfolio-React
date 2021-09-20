de2f6540880ce2c1405bc281578c6f4697f219c4
Jesse Sandvik <sandvikjesse@gmail.com>
2021-09-20 14:26:35 -0400

    added social icons

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,10 +2,13 @@ import "./Footer.scss";
 
 import React from "react";
 
+import SocialIcons from "../utils/Social-Icons";
+
 function Footer() {
     return (
         <footer className="footer">
             <h5 className="footer-copyright">Copyright &copy; 2021</h5>
+            <SocialIcons />
         </footer>
     );
 }
---
+++ src/utils/Social-Icons.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function SocialIcons() {
+    return (
+            <div className="social">
+                <a href="https://www.linkedin.com/in/jesse-sandvik-3b91b620a/" className="social-icon">
+                    <i className="fab fa-linkedin-in fa-2x"></i>
+                </a>
+                <a href="https://github.com/JesseSandvik" className="social-icon">
+                    <i className="fab fa-github fa-2x"></i>
+                </a>
+                <a href="mailto: sandvikjesse@gmail.com" className="social-icon">
+                    <i className="fas fa-at fa-2x"></i>
+                </a>
+            </div>
+    );
+}
+
+export default SocialIcons;
