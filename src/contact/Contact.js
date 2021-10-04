8f76371cd28086604af923bd075ebffcdc3a65cc
Jesse Sandvik <sandvikjesse@gmail.com>
2021-10-04 17:43:28 -0400

    added content to contact page

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -4,7 +4,25 @@ import React from "react";
 
 function Contact() {
     return (
-        <div className="contact">Contact</div>
+        <div className="contact">
+            <h1 className="contact-title">Contact</h1>
+            <h2 className="contact-section_title">Contact Me</h2>
+            <p className="contact-paraphrase">Contact me on social media or via email.</p>
+            <div className="contact-item">
+            <h3 className="contact-item_title">Email</h3>
+            <small>Email me directly!</small>
+            <a href="mailto: sandvikjesse@gmail.com">
+                <i className="fas fa-at icon"></i>
+            </a>
+            </div>
+            <div className="contact-item">
+            <h3 className="contact-item_title">LinkedIn</h3>
+            <small>Connect with me on linkedIn!</small>
+            <a href="https://www.linkedin.com/in/jesse-sandvik-3b91b620a/">
+                <i className="fab fa-linkedin-in icon"></i>
+            </a>
+            </div>
+        </div>
     );
 }
 
