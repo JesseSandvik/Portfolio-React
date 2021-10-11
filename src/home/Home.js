3ecee4efb4c9d899f324c9c1e5fdf32d3aacbec4
Jesse Sandvik <sandvikjesse@gmail.com>
2021-10-11 16:45:40 -0400

    fixed formatting for home page

--- src/home/Home.js
+++ src/home/Home.js
@@ -6,21 +6,17 @@ import DeveloperIcons from "../utils/DeveloperIcons";
 
 function Home() {
     return (
-        <div className="home">
-            <div className="home-intro">
+        <section className="home">
                 <p><b>Hello</b> and <b>welcome</b>!</p>
                 <small>My name is</small>
                 <h1><strong>Jesse Sandvik</strong>.</h1>
-                </div>
-                <div className="home-description">
                 <p>I am a </p>
                 <h2><strong>full-stack software developer</strong></h2>
                 <p>specializing in</p>
                 <h2><strong>front-end web development</strong>.</h2>
                 <DeveloperIcons />
                 <p>I'm here to help you solve problems, one line of code at a time.</p>
-            </div>
-        </div>
+        </section>
     );
 }
 
