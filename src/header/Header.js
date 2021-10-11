29155652cccee280706bdbaa8b9835cacd44e342
Jesse Sandvik <sandvikjesse@gmail.com>
2021-10-11 17:25:59 -0400

    refactored code for layout

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,7 +3,6 @@ import "./Header.scss";
 import React from "react";
 
 import HeaderLogo from "../headerLogo/HeaderLogo";
-import Navigation from "../navigation/Navigation";
 import SocialIcons from "../utils/SocialIcons";
 
 function Header() {
