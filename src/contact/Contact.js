9dd2226a4e95ffd8c703cd60326077f7f9d6a183
Jesse Sandvik <sandvikjesse@gmail.com>
2021-10-12 22:32:17 -0400

    added contact email form

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -2,6 +2,8 @@ import "./Contact.scss";
 
 import React from "react";
 
+import EmailForm from "../utils/EmailForm";
+
 function Contact() {
     return (
         <section className="contact">
@@ -10,14 +12,7 @@ function Contact() {
             <small>Connect with me on social media or send me an email!</small>
             <h3>Email</h3>
             <small>Email me directly!</small>
-            <a href="mailto: sandvikjesse@gmail.com" target="_blank" rel="noopener noreferrer">
-                <i className="fas fa-at icon"></i>
-            </a>
-            <h3>LinkedIn</h3>
-            <small>Connect with me on linkedIn!</small>
-            <a href="https://www.linkedin.com/in/jesse-sandvik-3b91b620a/" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-linkedin-in icon"></i>
-            </a>
+            <EmailForm />
         </section>
     );
 }
---
+++ src/utils/EmailForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function EmailForm() {
+    return (
+    <form action="https://submit-form.com/An0ce8MZ">
+        <label htmlFor="name">Name</label>
+        <input type="text"
+               id="name" 
+               name="name" 
+               placeholder="Name" 
+               required
+        />
+        <br />
+        <label htmlFor="email">Email</label>
+        <input type="email"
+               id="email" 
+               name="email" 
+               placeholder="Email" 
+               required
+        />
+        <br />
+        <label htmlFor="message">Message</label>
+        <textarea
+            id="message"
+            name="message"
+            placeholder="Message"
+            required
+            rows="20"
+            cols="60"
+            >
+        </textarea>
+        <button type="submit">Send</button>
+    </form>
+
+    );
+}
+
+export default EmailForm;
