ba52a4ff34a19660906340fc09ca0a525ca41521
Jesse Sandvik <sandvikjesse@gmail.com>
2021-10-12 22:40:31 -0400

    added test for email form

--- src/contact/__tests__/Contact.test.js
+++ src/contact/__tests__/Contact.test.js
@@ -4,14 +4,18 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact.js";
 
 describe("Contact", () => {
-    it("renders the Contact page", () => {
+    test("renders the h1 for Contact page", () => {
         render(<Contact />);
+
+        expect(screen.getByText(/Contact/)).toBeInTheDocument();
     });
 
-    it("renders Contact as an h1 element", () => {
+    test("renders email form", () => {
         render(<Contact />);
-        const elementValue = screen.getByText(/Contact/);
-        expect(elementValue).toBeInTheDocument();
+
+        expect(screen.getByLabelText(/Name/)).toBeInTheDocument();
+        expect(screen.getByLabelText(/Email/)).toBeInTheDocument();
+        expect(screen.getByLabelText(/Message/)).toBeInTheDocument();
+        expect(screen.getByText(/Send/)).toBeInTheDocument();
     });
-    
 });
