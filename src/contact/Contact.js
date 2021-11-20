b783b7a9663cd12d87ecee968816050020424e6f
Jesse Sandvik <sandvikjesse@gmail.com>
2021-11-19 22:08:15 -0500

    added twitter link to social icons

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -2,14 +2,9 @@ import './Contact.css';
 import React from "react";
 import { motion } from 'framer-motion';
 import EmailForm from "../utils/EmailForm";
-import contactOne from '../images/contact.png';
 import ScrollToTop from '../utils/ScrollToTop';
 
 export default function Contact({ initialMotion, animateMotion, exitMotion, transition }) {
-    const buttonLinkHandler = () => {
-        window.open("https://www.linkedin.com/in/jesse-sandvik/");
-    };
-
     return (
         <>
         <ScrollToTop />
@@ -21,15 +16,6 @@ export default function Contact({ initialMotion, animateMotion, exitMotion, tran
             transition={transition}
         >
             <h1>Contact</h1>
-            <img src={contactOne} alt="Coffee and Keyboard" />
-            <h2>Connect With Me!</h2>
-            <p>I am always open to networking opportunities!</p>
-            <p>Feel free to connect with me or send me a message. I'm usually very responsive and should get back to you within a day or so.</p>
-            <button title="https://www.linkedin.com/in/jesse-sandvik/" onClick={buttonLinkHandler} >
-                    <i className="fab fa-linkedin-in"></i>
-                    <small>https://www.linkedin.com/in/jesse-sandvik/</small>
-            </button>
-            <h2>Email Me Directly!</h2>
             <p>I am usually very responsive, you should expect to hear back from me within 12 to 24 hours of receiving your message.</p>
             <EmailForm />
         </motion.article>
--- src/utils/SocialIcons.js
+++ src/utils/SocialIcons.js
@@ -3,13 +3,16 @@ import React from 'react';
 export default function SocialIcons() {
     return (
         <>
-            <a title="linkedin link" href="https://www.linkedin.com/in/jesse-sandvik-3b91b620a/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/jesse-sandvik-3b91b620a/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-linkedin-in"></i>
             </a>
-            <a title="github link" href="https://github.com/JesseSandvik" target="_blank" rel="noopener noreferrer">
+            <a href="https://twitter.com/SandvikJesse" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-twitter"></i>
+            </a>
+            <a href="https://github.com/JesseSandvik" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-github"></i>
             </a>
-            <a title="email link" href="mailto: sandvikjesse@gmail.com" target="_blank" rel="noopener noreferrer">
+            <a href="mailto: sandvikjesse@gmail.com" target="_blank" rel="noopener noreferrer">
                 <i className="fas fa-at"></i>
             </a>
         </>
