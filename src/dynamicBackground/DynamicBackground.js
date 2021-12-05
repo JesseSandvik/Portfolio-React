adefe22f8273007b210c2e15c9171f08e59b38d8
Jesse Sandvik <sandvikjesse@gmail.com>
2021-12-05 15:45:18 -0500

    restyled navigation

--- src/dynamicBackground/DynamicBackground.js
+++ src/dynamicBackground/DynamicBackground.js
@@ -1,27 +1,77 @@
 import "./DynamicBackground.scss";
 import React from "react";
+import { useLocation } from "react-router";
 import classNames from "../utils/ClassNames";
 
 function DynamicBackground(props) {
+    const location = useLocation();
+
     return (
         <div className="bg">
             <div className={classNames({
                 "item bg1": props.backgroundToggle,
                 "item bg1 open": !props.backgroundToggle,
-            })}></div>
+            })}
+            >
+            </div>
             <div className={classNames({
                 "item bg2": props.backgroundToggle,
                 "item bg2 open": !props.backgroundToggle,
-            })}>
+            })}
+            >
+                {location.pathname === "/" && (
+                    <div className="page home">
+                    <p>H</p>
+                    <p>O</p>
+                    <p>M</p>
+                    <p>E</p>
+                    </div>
+                )}
+                {location.pathname === "/about" && (
+                    <div className="page about">
+                    <p>A</p>
+                    <p>B</p>
+                    <p>O</p>
+                    <p>U</p>
+                    <p>T</p>
+                    </div>
+                )}
+                {location.pathname === "/projects" && (
+                    <div className="page projects">
+                    <p>P</p>
+                    <p>R</p>
+                    <p>O</p>
+                    <p>J</p>
+                    <p>E</p>
+                    <p>C</p>
+                    <p>T</p>
+                    <p>S</p>
+                    </div>
+                )}
+                {location.pathname === "/contact" && (
+                    <div className="page contact">
+                    <p>C</p>
+                    <p>O</p>
+                    <p>N</p>
+                    <p>T</p>
+                    <p>A</p>
+                    <p>C</p>
+                    <p>T</p>
+                    </div>
+                )}
             </div>
             <div className={classNames({
                 "item bg3": props.backgroundToggle,
                 "item bg3 open": !props.backgroundToggle,
-            })}></div>
+            })}
+            >
+            </div>
             <div className={classNames({
                 "item bg4": props.backgroundToggle,
                 "item bg4 open": !props.backgroundToggle,
-            })}></div>
+            })}
+            >
+            </div>
         </div>
     );
 }
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,16 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Navigation() {
-
-    const navigationLinks = {
-        homeActive: true,
-        aboutActive: false,
-        projectsActive: false,
-        contactActive: false,
-    };
-
-    const [navigationLinkActive, setNavigationLinkActive] = useState(navigationLinks);
 
     return (
         <nav>
@@ -18,58 +9,34 @@ export default function Navigation() {
                 <li>
                     <NavLink
                         activeClassName="link-active"
-                        onClick={() => setNavigationLinkActive({
-                            homeActive: true,
-                            aboutActive: false,
-                            projectsActive: false,
-                            contactActive: false,
-                        })}
                         exact={true}
                         to="/"
                         >
-                        Home{navigationLinkActive.homeActive && <i className="fas fa-code"></i>}
+                        Home
                     </NavLink>
                 </li>
                 <li>
                     <NavLink
                         activeClassName="link-active"
-                        onClick={() => setNavigationLinkActive({
-                            homeActive: false,
-                            aboutActive: true,
-                            projectsActive: false,
-                            contactActive: false,
-                        })}
                         to="/about"
                         >
-                        About{navigationLinkActive.aboutActive && <i className="fas fa-code"></i>}
+                        About
                     </NavLink>
                 </li>
                 <li>
                     <NavLink
                     activeClassName="link-active"
-                    onClick={() => setNavigationLinkActive({
-                        homeActive: false,
-                        aboutActive: false,
-                        projectsActive: true,
-                        contactActive: false,
-                    })}
                     to="/projects"
                     >
-                    Projects{navigationLinkActive.projectsActive && <i className="fas fa-code"></i>}
+                    Projects
                     </NavLink>
                 </li>
                 <li>
                     <NavLink
                     activeClassName="link-active"
-                    onClick={() => setNavigationLinkActive({
-                        homeActive: false,
-                        aboutActive: false,
-                        projectsActive: false,
-                        contactActive: true,
-                    })}
                     to="/contact"
                     >
-                    Contact{navigationLinkActive.contactActive && <i className="fas fa-code"></i>}
+                    Contact
                     </NavLink>
                 </li>
             </ul>
